cb0fc23a42adb01c6b9ea88af3e71d824001eded
Vandivier <vandivi3r@gmail.com>
2026-01-05 17:52:59 -0500

    feat: analytics

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Work_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const headingFont = Cormorant_Garamond({
@@ -20,6 +21,9 @@ export const metadata: Metadata = {
     "Join the waitlist for The Evergreen Series, an epic fantasy book series.",
 };
 
+const gaId = process.env.NEXT_PUBLIC_GA_ID;
+const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,7 +31,43 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {gaId ? (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+            />
+            <Script id="ga4-init" strategy="afterInteractive">
+              {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${gaId}');`}
+            </Script>
+          </>
+        ) : null}
+        {gtmId ? (
+          <Script id="gtm-init" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+          </Script>
+        ) : null}
+      </head>
       <body className={`${headingFont.variable} ${bodyFont.variable} antialiased`}>
+        {gtmId ? (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+              title="gtm"
+            />
+          </noscript>
+        ) : null}
         {children}
       </body>
     </html>
